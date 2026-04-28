b5d8879ac02a66a68aaa4b830121bb6748e788e4
WhoFoss <willscheyler@gmail.com>
2026-04-28 18:52:48 -0300

    Update banner.sh

--- shell-config/config-files/banner.sh
+++ shell-config/config-files/banner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 img2sixel -w 500 ~/.config/neofetch/eu.png
 printf '\033[16A'
-COLUMNS=28 neofetch --backend off 2>/dev/null | whio
+COLUMNS=28 neofetch --backend off 2>/dev/null | while IFS= read -r line; do
     printf '\033[28C%s\n' "$line"
 done
 printf '\033[?25h'
