c5f12d0b12fe1e6dc1004017b78d439ed003d2e8
WhoFoss <willscheyler@gmail.com>
2026-04-28 21:05:21 -0300

    Delete shell-config/config-files/.banner.sh

--- shell-config/config-files/.banner.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-img2sixel -w 500 ~/.config/neofetch/eu.png
-printf '\033[16A'
-COLUMNS=28 neofetch --backend off 2>/dev/null | while IFS= read -r line; do
-    printf '\033[28C%s\n' "$line"
-done
-printf '\033[?25h\033[A'
